44a657ec70d8d3046632055c10870f270d490341
Мавлетов <mavletov@inbox.ru>
2023-12-18 20:38:18 +0300

    sitypro1812

--- README.md
+++
@@ -1,2 +0,0 @@
-# my_html_projects
-A.Dudalko_lecture
---
+++ sitipro/копии/181223/4/js/main.js
@@ -0,0 +1,82 @@
+const swiper = new Swiper('.mySwiper1', {
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+
+    clickable: true
+  },
+
+  a11y: {
+    paginationBulletMessage: 'Слайд номер{{index}}'
+  }
+
+
+});
+
+
+window.addEventListener('resize', function (){
+if (document.documentElement.clientWidth > 1200) {
+  var myswiper = new Swiper(".mySwiper2", {
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'fraction',
+
+      clickable: true
+    },
+
+    a11y: {
+      paginationBulletMessage: 'Слайд номер{{index}}'
+    }
+  });
+}
+else {
+  var myswiper = new Swiper(".mySwiper2", {
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+
+      clickable: true
+    },
+
+    a11y: {
+      paginationBulletMessage: 'Слайд номер{{index}}'
+    }
+  });
+}
+});
+
+ymaps.ready(function () {
+  var myMap = new ymaps.Map('map', {
+    center: [55.669006, 37.409657],
+    zoom: 17
+  }, {
+    searchControlProvider: 'yandex#search'
+  }),
+    myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+      hintContent: 'Москва, ул. Московская д. 0',
+    }, {
+
+      iconImageHref: 'img/Subtract.svg',
+      iconLayout: 'default#image',
+      iconImageSize: [28, 40],
+      iconImageOffset: [-5, -38]
+    });
+  myMap.geoObjects
+    .add(myPlacemark);
+});
+
